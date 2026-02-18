e3ca58925f69d36202b11e1530d8bbb90fee6b8a
kpxv <aden@sessionkey.net>
2026-02-18 11:00:39 -0500

    cmpe250 lab5 start

---
+++ cmpe-250/lab/lab5/prelab.s
@@ -0,0 +1,59 @@
+.set uart0_bdh, 0x4006_A000
+.set uart0_bdl, 0x4006_A001
+.set uart0_c1, 0x4006_A002
+.set uart0_c2, 0x4006_A003
+.set uart0_s1, 0x4006_A004
+.set uart0_s2, 0x4006_A005
+.set uart0_c3, 0x4006_A006
+.set uart0_d, 0x4006_A007
+.set uart0_ma1, 0x4006_A008
+.set uart0_ma2, 0x4006_A009
+.set uart0_c4, 0x4006_A00A
+.set uart0_c5, 0x4006_A00B
+
+.set uart0_bdh_9600, 0x01
+.set uart0_bdl_9600, 0x38
+.set uart0_c1_opt, 0x00
+
+.set uart0_c2_t_r_clr, 0x00
+.set uart0_c2_t_en, 0x08
+.set uart0_c2_r_en, 0x04
+.set uart0_c2_t_r_en, 0x0c
+
+
+.global _start
+
+_start:
+    bl Init_UART0_Polling
+    b .
+
+/**
+  * Initialize board for polled serial I/O with UART0 through ports B pins 1
+  * and 2, using: 8 data bits, no parity, and one stop bit at 9600 baud
+  * Changes: LR, PC, PSR
+  **/
+Init_UART0_Polling:
+    push {r0, r1}
+    // Clear TE and RE
+    movs r0, =uart0_c2
+    movs r1, uart0_c2_t_r_clr
+    strb r1, [r0, #0]
+
+    pop {r0, r1}
+    bx lr
+
+/**
+  * Gets a character from ____
+  * Return value in R0
+  * Changes: R0, LR, PC, PSR
+  **/
+GetChar:
+    bx lr
+
+/**
+  * Puts a character into ____
+  * Reads from R0
+  * Changes: LR, PC, PSR
+  **/
+PutChar:
+    bx lr
