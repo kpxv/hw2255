47f1806cf6e9872234a54f6eb8a6c218ba161417
kpxv <aden@sessionkey.net>
2026-02-19 02:38:33 -0500

    cmpe250 lab5 fixed equates

--- cmpe-250/lab/lab5/ProgramTemplate_KL05.s
+++ cmpe-250/lab/lab5/ProgramTemplate_KL05.s
@@ -21,58 +21,58 @@
 ;****************************************************************
 ;EQUates
 // Address EQUates
-.set uart0_bdh, 0x4006A000
-.set uart0_bdl, 0x4006A001
-.set uart0_c1, 0x4006A002
-.set uart0_c2, 0x4006A003
-.set uart0_s1, 0x4006A004
-.set uart0_s2, 0x4006A005
-.set uart0_c3, 0x4006A006
-.set uart0_d, 0x4006A007
-.set uart0_ma1, 0x4006A008
-.set uart0_ma2, 0x4006A009
-.set uart0_c4, 0x4006A00A
-.set uart0_c5, 0x4006A00B
+uart0_bdh EQU 0x4006A000
+uart0_bdl EQU 0x4006A001
+uart0_c1 EQU 0x4006A002
+uart0_c2 EQU 0x4006A003
+uart0_s1 EQU 0x4006A004
+uart0_s2 EQU 0x4006A005
+uart0_c3 EQU 0x4006A006
+uart0_d EQU 0x4006A007
+uart0_ma1 EQU 0x4006A008
+uart0_ma2 EQU 0x4006A009
+uart0_c4 EQU 0x4006A00A
+uart0_c5 EQU 0x4006A00B
 
-.set sim_sopt1, 0x40047000
-.set sim_sopt1cfg, 0x40047004
-.set sim_sopt2, 0x40048004
-.set sim_sopt4, 0x4004800C
-.set sim_sopt5, 0x40048010
-.set sim_sopt7, 0x40048018
-.set sim_sdid, 0x40048024
-.set sim_scgc4, 0x40048034
-.set sim_scgc5, 0x40048038
-.set sim_scgc6, 0x4004803C
-.set sim_scgc7, 0x40048040
-.set sim_clkdiv1, 0x40048044
-.set sim_fcfg1, 0x4004804c
-.set sim_fcfg2, 0x40048050
-.set sim_uidmh, 0x40048058
-.set sim_uidml, 0x4004805c
-.set sim_uidl, 0x40048060
-.set sim_copc, 0x40048100
-.set sim_srvcop, 0x40048104
+sim_sopt1 EQU 0x40047000
+sim_sopt1cfg EQU 0x40047004
+sim_sopt2 EQU 0x40048004
+sim_sopt4 EQU 0x4004800C
+sim_sopt5 EQU 0x40048010
+sim_sopt7 EQU 0x40048018
+sim_sdid EQU 0x40048024
+sim_scgc4 EQU 0x40048034
+sim_scgc5 EQU 0x40048038
+sim_scgc6 EQU 0x4004803C
+sim_scgc7 EQU 0x40048040
+sim_clkdiv1 EQU 0x40048044
+sim_fcfg1 EQU 0x4004804c
+sim_fcfg2 EQU 0x40048050
+sim_uidmh EQU 0x40048058
+sim_uidml EQU 0x4004805c
+sim_uidl EQU 0x40048060
+sim_copc EQU 0x40048100
+sim_srvcop EQU 0x40048104
 
 // Shift EQUates
-.set sim_sopt2_uart0src_shift, 26
+sim_sopt2_uart0src_shift EQU 26
 
 // Setting EQUates
-.set uart0_bdh_9600, 0x01
-.set uart0_bdl_9600, 0x38
+uart0_bdh_9600 EQU 0x01
+uart0_bdl_9600 EQU 0x38
 
-.set uart0_c1_opt, 0x00
+uart0_c1_opt EQU 0x00
 
-.set uart0_c2_t_r_clr, 0x00
-.set uart0_c2_t_en, 0x08
-.set uart0_c2_r_en, 0x04
-.set uart0_c2_t_r_en, 0x0c
+uart0_c2_t_r_clr EQU 0x00
+uart0_c2_t_en EQU 0x08
+uart0_c2_r_en EQU 0x04
+uart0_c2_t_r_en EQU 0x0c
 
-.set sim_sopt2_uart0src_mcgfllclk, (01 << sim_sopt2_uart0src_shift)
+sim_sopt2_uart0src_mcgfllclk EQU (01 << sim_sopt2_uart0src_shift)
 
 // Mask EQUates
-.set uart0_s1_tdre_mask, 0x80
-.set uart0_s1_rdrf_mask, 0x20
+uart0_s1_tdre_mask EQU 0x80
+uart0_s1_rdrf_mask EQU 0x20
 
 
 // Stores \data to \dest. Modifies R0 and R1
