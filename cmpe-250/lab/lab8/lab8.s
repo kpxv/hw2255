cd709206f08c05445de3ec221837f121059efd36
kpxv <aden@sessionkey.net>
2026-03-25 22:35:41 -0400

    cmpe250 lab8 progress

---
+++ cmpe-250/lab/lab8/lab8.s
@@ -0,0 +1,258 @@
+.set n, 8
+.set stack_buf_sz, 8*n+4
+
+.set hex_0, 0x30
+.set hex_9, 0x3A
+.set hex_A, 0x41
+.set hex_a, 0x61
+.set hex_a_A, 0x20
+.set hex_A_10, 0x7
+
+.section .text
+.global _start
+_start:
+    ldr r0, =str
+    bl ReverseString
+    b .
+    ldr r0, =sum
+    ldr r1, =a1
+    ldr r2, =a2
+    movs r3, #n
+    bl AddIntMultiU
+    movs r1, #n
+    bl PutHexIntMulti
+    b .
+
+
+
+/**
+  * Adds n unsigned words together
+  *
+  * Inputs:
+  *     r0 : address to store sum
+  *     r1 : start address of the first addend
+  *     r2 : start address of the second addend
+  *     r3 : the length n of the numbers
+  * Outputs:
+  *     psr : clear c iff valid n-word number returned
+  * Modifies:
+  *     psr
+  */
+AddIntMultiU:
+    push {r3-r6}
+    //  r4 : addend store / sum
+    //  r5 : addend store
+    //  r6 : hold apsr
+    lsls r3, #2
+    // Clear carry
+    adds r0, r0, #0
+    mrs r6, apsr
+aimu_loop:
+    subs r3, r3, #4
+    ldr r4, [r1, r3]
+    ldr r5, [r2, r3]
+    // Restore apsr flags
+    msr apsr, r6
+    adcs r4, r4, r5
+    // Store apsr flags
+    mrs r6, apsr
+    str r4, [r0, r3]
+    cmp r3, #0
+    bne aimu_loop
+    // Set C if addition overflowed
+    msr apsr, r6
+    pop {r3-r6}
+    bx lr
+
+
+
+/**
+  * Gets an n-word ASCII-encoded hex number from UART, terminated on return
+  * keystroke, and stores it as binary in memory
+  *
+  * TODO: Use GetStringSB. Prefer allocate string buffer on stack.
+  * TODO: Extra credit: Allow enter only significant digits of number.
+  * TODO: Extra credit: Allow upper or lowercase alpha characters.
+  *
+  * Inputs:
+  *     r0 : address to store binary number
+  *     r1 : the length n of the number
+  * Outputs:
+  *     psr : clear c iff valid n-word number returned
+  * Modifies:
+  *     psr
+  */
+GetHexIntMulti:
+    push {lr}
+    // r4 : store address
+    // r5 : length n
+    // r6 : stack pointer
+    movs r4, r0
+    movs r5, r1
+    // Use stack as buffer
+    // In Thumb 1, cannot easily make dynamic buffer in stack. Uses an EQUate instead.
+    sub sp, #stack_buf_sz
+    mov r0, sp
+    // Find length of string buffer
+    lsls r1, #3
+    adds r1, #1
+    // Get input from user
+    bl GetStringSB
+    // Put LSB closest to the stack pointer
+    bl ReverseString
+
+    mov r6, sp
+    movs r7, #0
+    subs r1, #2
+ghim_store_in:
+    ldrb r0, [r6, r7]
+    cmp r0, #0
+    beq ghim_end_store
+    bl HexToBin
+    bcs ghim_exit
+    lsls r0, #4
+
+ghim_exit:
+    add sp, #stack_buf_sz
+    pop {pc}
+
+
+
+/**
+  * Outputs an n-word ASCII-encoded hex number to UART
+  *
+  * Inputs:
+  *     r0 : start address of number
+  *     r1 : the length n of the number
+  * Outputs:
+  *     None
+  * Modifies:
+  *     psr
+  */
+PutHexIntMulti:
+    push {r0-r1, r4, lr}
+    // r0 : arg for PutNumHex
+    // r1 : loop counter
+    // r4 : word address
+    movs r4, r0
+phim_loop:
+    // Print the ith word
+    ldr r0, [r4]
+    bl PutNumHex
+    // Increment counters
+    adds r4, #4
+    subs r1, #1
+    // Loop
+    cmp r1, #0
+    bne phim_loop
+    pop {r0-r1, r4, pc}
+
+
+
+/**
+  * Placeholder
+  */
+PutNumHex:
+    movs r7, r0
+    bx lr
+
+
+
+/**
+  * Placeholder
+  */
+GetStringSB:
+    bx lr
+
+
+
+/**
+  * Reverses a nul-terminated string in place
+  *
+  * Inputs:
+  *     r0 : input string address
+  * Outputs:
+  *     None
+  * Modifies:
+  *     psr
+  */
+ReverseString:
+    push {r4-r7}
+    // r0 : input address
+    // r4 : input offset
+    // r5 : output offset
+    // r6 : temporary byte storage
+    // r7 : temporary byte storage
+    movs r4, #0
+    movs r5, #0
+    // Search for NUL terminator
+rs_find_nul:
+    ldrb r6, [r0, r4]
+    adds r4, #1
+    cmp r6, #0
+    bne rs_find_nul
+    subs r4, #2
+rs_reverse:
+    // Swap bytes from either end of the string
+    ldrb r6, [r0, r4]
+    ldrb r7, [r0, r5]
+    strb r6, [r0, r5]
+    strb r7, [r0, r4]
+    subs r4, #1
+    adds r5, #1
+    cmp r4, r5
+    bge rs_reverse
+    pop{r4-r7}
+    bx lr
+
+
+
+/**
+  * Convert an ASCII char byte to a binary nibble.
+  *
+  * Inputs:
+  *     r0 : byte to convert
+  * Outputs:
+  *     psr : clear c iff valid input
+  * Modifies:
+  *     psr
+  */
+HexToBin:
+    // Must be at least hex 0
+    cmp r0, #hex_0
+    blo htb_fail
+    // Must not be between hex 9 and hex A
+    cmp r0, #hex_9
+    blo htb_tolower
+    cmp r0, #hex_A
+    blo htb_fail
+htb_tolower:
+    cmp r0, #hex_a
+    blo htb_tooffsetbin
+    subs r0, #hex_a_A
+htb_tooffsetbin:
+    cmp r0, #hex_A
+    blo htb_tobin
+    subs r0, #hex_A_10
+htb_tobin:
+    subs r0, #hex_0
+    cmp r0, #0xF
+    bht htb_fail
+htb_pass:
+    // Clear C flag
+    adds r0, #0
+    b htb_exit
+htb_fail:
+    // Set C flag
+    movs r0, #1
+    subs r0, #1
+htb_exit:
+    bx lr
+
+
+
+.section .data
+sum: .word 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000
+a1: .word 0x00001111, 0x00002222, 0x00003333, 0x00004444, 0x00005555, 0x00006666, 0x00007777, 0x00008888
+a2: .word 0x00100000, 0x00200000, 0x00300000, 0x00400000, 0x00500000, 0x00600000, 0x00700000, 0x00800000
+str: .byte 'a', 'b', 'c', 'd', 'e', 'f', 0x00
