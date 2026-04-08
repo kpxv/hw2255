bf0dc5e446b3f40ac610f2bd3052de27961ddd4c
kpxv <aden@sessionkey.net>
2026-04-08 11:16:48 -0400

    cmpe250 lab9 sync

--- cmpe-250/lab/lab9/lab9.s
+++ cmpe-250/lab/lab9/lab9.s
@@ -20,6 +20,20 @@
 .set UART0_S1_CLEAR_FLAGS  ,  (UART0_S1_IDLE_MASK | UART0_S1_OR_MASK |    UART0_S1_NF_MASK |  UART0_S1_FE_MASK | UART0_S1_PF_MASK) 
 .set UART0_S2_NO_RXINV_BRK10_NO_LBKDETECT_CLEAR_FLAGS  ,  (UART0_S2_LBKDIF_MASK | UART0_S2_RXEDGIF_MASK) 
 
+// Offsets from base record
+.set in_ptr, 0
+.set out_ptr, 4
+.set buf_start, 8
+.set buf_past, 12
+.set buf_size, 16
+.set num_enqd, 17
+
+// Sizes of buffers and records
+.set rx_qbuf_sz, 80
+.set rx_qrec_sz, 18
+.set tx_qbuf_sz, 80
+.set tx_qrec_sz, 18
+
 .section code, "ax"
 .global Reset_Handler
 Reset_Handler:
@@ -33,72 +47,111 @@ UART0_ISR:
     bx lr
 
 Init_UART0_IRQ:
-            PUSH {R0, R1, R2, LR}
+            push {r0, r1, r2, lr}
+            // Initialize queues
+            ldr r0, =rx_buffer
+            ldr r1, =rx_record
+            movs r2, #rx_qbuf_sz
             bl init_queue
+            ldr r0, =tx_buffer
+            ldr r1, =tx_record
             bl init_queue
             // Select MCGFLLCLK as UART0 clock source 
-            LDR   R0,=SIM_SOPT2 
-            LDR   R1,=SIM_SOPT2_UART0SRC_MASK 
-            LDR   R2,[R0,#0] 
-            BICS  R2,R2,R1 
-            LDR   R1,=SIM_SOPT2_UART0SRC_MCGFLLCLK 
-            ORRS  R2,R2,R1 
-            STR   R2,[R0,#0] 
+            ldr   r0,=SIM_SOPT2 
+            ldr   r1,=SIM_SOPT2_UART0SRC_MASK 
+            ldr   r2,[r0,#0] 
+            bics  r2,r2,r1 
+            ldr   r1,=SIM_SOPT2_UART0SRC_MCGFLLCLK 
+            orrs  r2,r2,r1 
+            str   r2,[r0,#0] 
             // Set UART0 for external connection 
-            LDR   R0,=SIM_SOPT5 
-            LDR   R1,=SIM_SOPT5_UART0_EXTERN_MASK_CLEAR 
-            LDR   R2,[R0,#0] 
-            BICS  R2,R2,R1 
-            STR   R2,[R0,#0] 
+            ldr   r0,=SIM_SOPT5 
+            ldr   r1,=SIM_SOPT5_UART0_EXTERN_MASK_CLEAR 
+            ldr   r2,[r0,#0] 
+            bics  r2,r2,r1 
+            str   r2,[r0,#0] 
             // Enable UART0 module clock 
-            LDR   R0,=SIM_SCGC4 
-            LDR   R1,=SIM_SCGC4_UART0_MASK 
-            LDR   R2,[R0,#0] 
-            ORRS  R2,R2,R1 
-            STR   R2,[R0,#0] 
+            ldr   r0,=SIM_SCGC4 
+            ldr   r1,=SIM_SCGC4_UART0_MASK 
+            ldr   r2,[r0,#0] 
+            orrs  r2,r2,r1 
+            str   r2,[r0,#0] 
             // Enable PORT B module clock 
-            LDR   R0,=SIM_SCGC5 
-            LDR   R1,=SIM_SCGC5_PORTB_MASK 
-            LDR   R2,[R0,#0] 
-            ORRS  R2,R2,R1 
-            STR   R2,[R0,#0] 
+            ldr   r0,=SIM_SCGC5 
+            ldr   r1,=SIM_SCGC5_PORTB_MASK 
+            ldr   r2,[r0,#0] 
+            orrs  r2,r2,r1 
+            str   r2,[r0,#0] 
             // Select PORT B Pin 2 (D0) for UART0 RX (J8 Pin 01) 
-            LDR     R0,=PORTB_PCR2 
-            LDR     R1,=PORT_PCR_SET_PTB2_UART0_RX 
-            STR     R1,[R0,#0] 
+            ldr     r0,=PORTB_PCR2 
+            ldr     r1,=PORT_PCR_SET_PTB2_UART0_RX 
+            str     r1,[r0,#0] 
             //  Select PORT B Pin 1 (D1) for UART0 TX (J8 Pin 02) 
-            LDR     R0,=PORTB_PCR1 
-            LDR     R1,=PORT_PCR_SET_PTB1_UART0_TX 
-            STR     R1,[R0,#0] 
+            ldr     r0,=PORTB_PCR1 
+            ldr     r1,=PORT_PCR_SET_PTB1_UART0_TX 
+            str     r1,[r0,#0] 
             // Disable UART0 receiver and transmitter 
-            LDR   R0,=UART0_BASE 
-            MOVS  R1,#UART0_C2_T_R 
-            LDRB  R2,[R0,#UART0_C2_OFFSET] 
-            BICS  R2,R2,R1 
-            STRB  R2,[R0,#UART0_C2_OFFSET] 
+            ldr   r0,=UART0_BASE 
+            movs  r1,#UART0_C2_T_R 
+            ldrb  r2,[r0,#UART0_C2_OFFSET] 
+            bics  r2,r2,r1 
+            strb  r2,[r0,#UART0_C2_OFFSET] 
             // Set UART0 for 9600 baud, 8N1 protocol 
-            MOVS  R1,#UART0_BDH_9600 
-            STRB  R1,[R0,#UART0_BDH_OFFSET] 
-            MOVS  R1,#UART0_BDL_9600 
-            STRB  R1,[R0,#UART0_BDL_OFFSET] 
-            MOVS  R1,#UART0_C1_8N1 
-            STRB  R1,[R0,#UART0_C1_OFFSET] 
-            MOVS  R1,#UART0_C3_NO_TXINV 
-            STRB  R1,[R0,#UART0_C3_OFFSET] 
-            MOVS  R1,#UART0_C4_NO_MATCH_OSR_16 
-            STRB  R1,[R0,#UART0_C4_OFFSET] 
-            MOVS  R1,#UART0_C5_NO_DMA_SSR_SYNC 
-            STRB  R1,[R0,#UART0_C5_OFFSET] 
-            MOVS  R1,#UART0_S1_CLEAR_FLAGS 
-            STRB  R1,[R0,#UART0_S1_OFFSET] 
-            MOVS  R1, #UART0_S2_NO_RXINV_BRK10_NO_LBKDETECT_CLEAR_FLAGS 
-            STRB  R1,[R0,#UART0_S2_OFFSET] 
+            movs  r1,#UART0_BDH_9600 
+            strb  r1,[r0,#UART0_BDH_OFFSET] 
+            movs  r1,#UART0_BDL_9600 
+            strb  r1,[r0,#UART0_BDL_OFFSET] 
+            movs  r1,#UART0_C1_8N1 
+            strb  r1,[r0,#UART0_C1_OFFSET] 
+            movs  r1,#UART0_C3_NO_TXINV 
+            strb  r1,[r0,#UART0_C3_OFFSET] 
+            movs  r1,#UART0_C4_NO_MATCH_OSR_16 
+            strb  r1,[r0,#UART0_C4_OFFSET] 
+            movs  r1,#UART0_C5_NO_DMA_SSR_SYNC 
+            strb  r1,[r0,#UART0_C5_OFFSET] 
+            movs  r1,#UART0_S1_CLEAR_FLAGS 
+            strb  r1,[r0,#UART0_S1_OFFSET] 
+            movs  r1, #UART0_S2_NO_RXINV_BRK10_NO_LBKDETECT_CLEAR_FLAGS 
+            strb  r1,[r0,#UART0_S2_OFFSET] 
             // Enable UART0 receiver and transmitter 
-            MOVS  R1,#UART0_C2_T_R 
-            STRB  R1,[R0,#UART0_C2_OFFSET] 
+            movs  r1,#UART0_C2_T_R 
+            strb  r1,[r0,#UART0_C2_OFFSET] 
 
-            POP {R0, R1, R2}
-            BX LR
+            pop {r0, r1, r2}
+            bx lr
+
+
+/** Initialize queue record
+  *
+  * Inputs:
+  *     R0 : buffer address
+  *     R1 : record address
+  *     R2 : buffer capacity
+  * Outputs:
+  *     None
+  * Modified:
+  *     psr
+  */
+init_queue:
+            push {r3}
+            // In pointer
+            str r0, [r1, #in_ptr]
+            // Out pointer
+            str r0, [r1, #out_ptr]
+            // Buffer start
+            str r0, [r1, #buf_start]
+            // Buffer end
+            adds r3, r0, r2
+            subs r3, #1
+            str r3, [r1, #buf_past]
+            // Buffer size
+            strb r2, [r1, #buf_size]
+            // Number enqueued
+            movs r3, #0
+            strb r3, [r1, #num_enqd]
+            pop {r3}
+            bx lr
+
 
 // ****************************************************************
 // Vector Table Mapped to Address 0 at Reset
@@ -175,6 +228,16 @@ __Vectors_End:
 .section const, "a"
 test:
 .word 0x01234567
+
 .section data, "aw", %nobits
-test1:
-.skip 0x8
+rx_buffer:
+.skip rx_qbuf_sz
+rx_record:
+.skip rx_qrec_sz
+
+.balign 4
+
+tx_buffer:
+.skip tx_qbuf_sz
+tx_record:
+.skip tx_qrec_sz
